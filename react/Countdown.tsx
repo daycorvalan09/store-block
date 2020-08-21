0c460dd8c123778bd8e7d8abec99a76316111b84
daycorvalan09 <daycorvalan09@gmail.com>
2020-08-21 20:18:10 -0300

    enviando mudanças

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
+import { useQuery } from 'react-apollo'
+//@ts-ignore
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
-interface CountdownProps { }
+
+interface CountdownProps {
+  targetDate: string
+}
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
+const CSS_HANDLES = ["countdown"]
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
-}
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+   //const { product: { linkText } } = useProduct()
+  
+  
+   const productContext = useProduct()
+   const product = productContext?.product  
+   const linkText = product?.link
+ 
+   const { data, loading, error } = useQuery(productReleaseDate, {
+     
+     variables: {
+       slug: linkText    
+     },
+     
+     ssr: false
+   })
+  
+ 
+   tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+ 
+   if (loading) {
+     return (
+       <div>
+         <span>Loading...</span>
+       </div>
+     )
+   }
+   if (error) {
+     return (
+       <div>
+         <span>Erro!</span>
+       </div>
+     )
+   }
+   if (!product) {
+     return (
+       <div>
+         <span>Não há contexto de produto</span>
+       </div>
+     )
+   }
+ 
+   return (
+     <div className={`${handles.countdown} db tc`}>
+       {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+     </div>
+   )
+ }
+ 
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
